3f01eddd3416e9bc0018dbc65a55c3f987926901
harithachintha66 <chinthaharitha066@gmail.com>
2023-10-31 21:31:19 +0530

    commit

--- src/BookMyseat/BookingConfirm.jsx
+++ src/BookMyseat/BookingConfirm.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { useEffect } from 'react';
-const BookingConfirm = ({ selectedSeats, ticketType }) => {
-    
-    useEffect(() => {
-      console.log('Selected Seats:', selectedSeats);
-      console.log('Ticket Type:', ticketType);
-    }, [selectedSeats, ticketType]);
+const BookingConfirm = ({ selectedSeats, ticketType, price }) => {
+
   return (
     <div>
       <div className="confirmation-container">
@@ -13,6 +8,7 @@ const BookingConfirm = ({ selectedSeats, ticketType }) => {
       <p>Ticket Type: {ticketType}</p>
       <p>Number of Seats Selected: {selectedSeats.length}</p>
       <p>Selected Seats: {selectedSeats.join(', ')}</p>
+      <p>Price :{price}</p>
       <p>Thank you for your booking!</p>
     </div>
 
--- src/BookMyseat/Hallform.jsx
+++ src/BookMyseat/Hallform.jsx
@@ -11,7 +11,13 @@ const Hallform = () => {
     const [ticketPrice, setTicketPrice] = useState(0);
     const [total, setTotal] = useState(0);
     const [proceeded, setProceeded] = useState(false);
-  
+    const [seatCountAvailable, setSeatCountAvailable] = useState(100);
+    const [seatCountOccupied, setSeatCountOccupied] = useState(0);
+    const [confirmedSeats, setConfirmedSeats] = useState([]);
+    const [confirmedNumberOfSeats, setConfirmedNumberOfSeats] = useState(1);
+    const [confirmedPrice, setConfirmedPrice] = useState(0);
+    // const [showConfirmation, setShowConfirmation] = useState(false);
+
       useEffect(() => {
       const ticketSelect = document.getElementById('ticket');
       setTicketPrice(parseFloat(ticketSelect.value));
@@ -20,40 +26,54 @@ const Hallform = () => {
       useEffect(() => {
         setTotal(selectedSeats.length * ticketPrice);
       }, [selectedSeats, ticketPrice]);
+      console.log(total);
   
       useEffect(() => {
         const availableSeatsCount = seatStatus.filter(status => status === 'available').length;
+        setSeatCountAvailable(availableSeatsCount);
         const occupiedSeatsCount = seatStatus.filter(status => status === 'occupied').length;
-        console.log(`Available Seats: ${availableSeatsCount}`);
-        console.log(`Occupied Seats: ${occupiedSeatsCount}`);
+        setSeatCountOccupied(occupiedSeatsCount);
       }, [seatStatus]);
       
       const updateSelectedCount = () => {
         const seatsCount = selectedSeats.length;
         setTotal(seatsCount * ticketPrice);
       };
-
       const handleTicketTypeChange = (e) => {
-        setTicketPrice(parseFloat(e.target.value));
+        const newTicketPrice = parseFloat(e.target.value);
+        setTicketPrice(newTicketPrice);
+        const selectedSeatsCount = selectedSeats.length;
+        const newTotal = selectedSeatsCount * newTicketPrice;
+        setTotal(newTotal);
+        setSelectedSeats([]);
         updateSelectedCount();
       };
-
+     
       const handleSeatClick = (seatIndex) => {
+        
         if (seatStatus[seatIndex] === 'available') {
           const rowIndex = Math.floor(seatIndex / 10);
           const maxPremiumRows = 3;
           const isPremiumSeat = rowIndex < maxPremiumRows;
-      
-          if ((ticketPrice === 120 && isPremiumSeat) || (ticketPrice === 80 && !isPremiumSeat)) {
-            const updatedSeatStatus = [...seatStatus];
-            updatedSeatStatus[seatIndex] = 'selected';
-            setSeatStatus(updatedSeatStatus);
-            setSelectedSeats(prevSelectedSeats => [...prevSelectedSeats, seatIndex]);
+          if ((ticketPrice === 120 && isPremiumSeat) || (ticketPrice === 80 && !isPremiumSeat)) 
+          {
+            if (selectedSeats.length < numberOfSeats) 
+            {
+              const updatedSeatStatus = [...seatStatus];
+              updatedSeatStatus[seatIndex] = 'selected';
+              setSeatStatus(updatedSeatStatus);
+              setSelectedSeats(prevSelectedSeats => [...prevSelectedSeats, seatIndex]);
+            } 
+            else 
+            {
+              alert(`You can only select ${numberOfSeats} seats.`);
+            }
           } 
           else {
-            alert('Invalid seat selection for the selected ticket type. only first 3 rows for premium');
+            alert('Invalid seat selection for the selected ticket type. Only first 3 rows are for premium seats.');
           }
-        } else if (seatStatus[seatIndex] === 'selected') {
+        } 
+        else if (seatStatus[seatIndex] === 'selected') {
           const updatedSeatStatus = [...seatStatus];
           updatedSeatStatus[seatIndex] = 'available';
           setSeatStatus(updatedSeatStatus);
@@ -62,45 +82,59 @@ const Hallform = () => {
       };
 
       const handleProceedClick = () => {
-        const updatedSeatStatus = [...seatStatus];
-        selectedSeats.forEach(seatIndex => {
-        updatedSeatStatus[seatIndex] = 'occupied';
-        });
-        setSeatStatus(updatedSeatStatus);
-        setProceeded(true);
+        const selectedCount = selectedSeats.length;
+        if (selectedCount === numberOfSeats) {
+          const updatedSeatStatus = [...seatStatus];
+          selectedSeats.forEach(seatIndex => {
+            updatedSeatStatus[seatIndex] = 'occupied';
+          });
+          setSeatStatus(updatedSeatStatus);
+          setConfirmedSeats([...selectedSeats]);
+          setConfirmedNumberOfSeats(numberOfSeats);
+          setConfirmedPrice(total);
+          setProceeded(true);
+          setSelectedSeats([]);
+          setNumberOfSeats(1);
+        } else {
+          alert(`Please select exactly ${numberOfSeats} seats.`);
+        }
       };
+      
     
   return (
     <div>
       <h1 id='heading'>BookMySeat</h1>
-    <div className='ticketTypeContainer'>
-            <label htmlFor="ticket">
-            <select name="ticket" id="ticket" onChange={handleTicketTypeChange}>  
+    <div className='ticketAndquantity-Container'> 
+    <div className='ticketTypeContainer'> 
+            <select name="ticket" id="ticket" onChange={handleTicketTypeChange} style={{backgroundColor:'rgb(91, 157, 101)', color:'white', borderColor:'white'}}>  
+                <option>Ticket Type</option>
                 <option value="120">Premium(₹120)</option>
                 <option value="80">Standard(₹80)</option>
             </select>
-            </label>
-
-            <select
+             &nbsp;
+          <label htmlFor="quantity">Qty:</label>   
+          <select
           className="quantity"
           value={numberOfSeats}
-          onChange={(e) => setNumberOfSeats(parseInt(e.target.value, 10))}
-        >
+          onChange={(e) => setNumberOfSeats(parseInt(e.target.value, 10))} 
+          style={{backgroundColor:'cadetblue', color:'white', borderColor:'white'}}
+          >
           {[...Array(6).keys()].map((num) => (
             <option key={num} value={num + 1}>
               {num + 1}
             </option>
           ))}
         </select>
+      </div>   
         {/* list that shows availability of seats */}
         <ul className='showAvailability'>
             <li>
                 <div className="seat occupied"></div>
-                <span>Occupied</span>
+                <span>Occupied({seatCountOccupied})</span>
             </li>
             <li>
                 <div className="seat"></div>
-                <span>N/A</span>
+                <span>Available({seatCountAvailable})</span>
             </li>
             <li>
                 <div className="seat selected"></div>
@@ -125,15 +159,20 @@ const Hallform = () => {
           </div>
         ))}
         </div>
-      <p className="text">
-        You have selected <span id="count">{selectedSeats.length}</span> seats for a price of ₹<span id="total">{total}</span>
-      </p>
+      
       <div>
         <button className='proceed' onClick={handleProceedClick}>
             proceed
         </button>
       </div>
-      {proceeded && <BookingConfirm selectedSeats={selectedSeats} ticketType={ticketPrice === 120 ? 'Premium' : 'Standard'} />}
+      {proceeded ? 
+     (<BookingConfirm
+      selectedSeats={confirmedSeats}
+      ticketType={ticketPrice === 120 ? 'Premium' : 'Standard'}
+      price={confirmedPrice}
+      numberOfSeats={confirmedNumberOfSeats} // Pass the number of seats as a prop
+     />) : <></>}
+    
 
     </div>
     </div>
